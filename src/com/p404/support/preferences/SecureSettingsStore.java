0742b24ed16520e724fab6c9574cdedc72559611
SpiritCroc <spiritcroc@gmail.com>
2018-03-25 19:50:45 -0700

    Settings: Add SecureSettingListPreference
    Change-Id: Ife5512b88264debb2a21ecc64cdaf3622d38105b

--- src/com/p404/support/preferences/SecureSettingsStore.java
+++ src/com/p404/support/preferences/SecureSettingsStore.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package com.p404.support.preferences;
+package com.p404.settings.preferences;
 
 import android.content.ContentResolver;
 import android.preference.PreferenceDataStore;
